c80b8c7ebeed5845853a4f73b6eb3ab4e33d2e71
Prashanth <mudunuriprashanth143@gmail.com>
2025-07-25 21:22:23 +0530

    Added AdminDashboard login backend and frontend functionality

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (!mongoURL) {
   process.exit(1);
 }
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'15mb'}));
 MongoClient.connect(mongoURL)
   .then((client) => {
     console.log("✅ Database connected successfully");
@@ -84,6 +84,18 @@ MongoClient.connect(mongoURL)
       } else {
       res.status(404).send({ success: false }); // ✅ Send object
       }
+      });
+
+      app.post("/AdminDashboard",async(req,res)=>{
+        const payload=req.body;
+        const collections=db.collection("klimages");
+        const add=await collections.insertOne(payload);
+        if(add  && add.insertedId){
+          res.send({success:true,message:"Successfully added"});
+        }
+        else{
+          res.status(404).send({success:false,message:"image not added"});
+        }
       });
 
 
